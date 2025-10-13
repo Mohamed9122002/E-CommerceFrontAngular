db35f8c1f4722c6ea3bdbcb91c4e8efb8a98ad6c
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-14 01:26:43 +0300

    Validators

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,13 +10,14 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class RegisterComponent {
   registerForm:FormGroup =  new FormGroup({
-    name:new FormControl(null),
-    email:new FormControl(null),
-    password:new FormControl(null),
-    rePassword:new FormControl(null),
-    phone:new FormControl(null),
+    name:new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(30)]),
+    email:new FormControl(null,[Validators.email ,Validators.required]),
+    password:new FormControl(null,[Validators.required,Validators.pattern(/^\w{6,}$/)]),
+    rePassword:new FormControl(null,[Validators.required,Validators.pattern(/^\w{6,}$/)]),
+    phone:new FormControl(null,[Validators.required,Validators.pattern(/^01[0125][0-9]{8}$/)]),
   });
   registerFormSubmit():void {
+   console.log(this.registerForm.value);
     
   }
 }
