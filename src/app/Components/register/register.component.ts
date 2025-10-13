3579e67553459e9b0ad084dcdd53e5a691dd1431
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-14 02:28:54 +0300

    registerSendDataTOAPI

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../Core/Services/auth.service';
+import { Certificate } from 'crypto';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -9,6 +12,10 @@ import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validator
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  private readonly _authService= inject(AuthService)
+  msgError:string ="";
+  isLoading:boolean = false;
+
   registerForm:FormGroup =  new FormGroup({
     name:new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(30)]),
     email:new FormControl(null,[Validators.email ,Validators.required]),
@@ -17,11 +24,23 @@ export class RegisterComponent {
     phone:new FormControl(null,[Validators.required,Validators.pattern(/^01[0125][0-9]{8}$/)]),
   },this.confirmPassword);
   registerFormSubmit():void {
-   console.log(this.registerForm.value);
-    
+    this.isLoading = true;
+    if(this.registerForm.valid){
+      this._authService.setRegisterForm(this.registerForm.value).subscribe({
+        next:(res)=>{
+          console.log(res);
+          
+        },
+        error:(err:HttpErrorResponse)=>{
+          this.msgError = err.error.message 
+          console.log(err);
+          
+        }
+      })
+    }    
   }
   confirmPassword(g:AbstractControl){
-    if(g.get('password')?.valid == g.get('rePassword')?.value){
+    if(g.get('password')?.value === g.get('rePassword')?.value){
       return null;
     }else{
       return {mismatch:true}
---
+++ src/app/Core/Services/auth.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+private readonly _httpClient = inject(HttpClient);
+ setRegisterForm(data:object){
+ return this._httpClient.post("https://ecommerce.routemisr.com/api/v1/auth/signup",data)
+ }
+}
