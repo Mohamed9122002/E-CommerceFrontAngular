6c6542b747a6deb9e862c261f3998707758c04f1
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-14 16:56:15 +0300

    getAllProducts

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../Core/Services/products.service';
+import { HttpResponse } from '@angular/common/http';
+import { IProduct } from '../../Core/Interface/iproduct';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,22 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private readonly _productService = inject(ProductsService);
+  productList:IProduct[] = []
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this._productService.getAllProducts().subscribe({
+      next: (res) => {
+        console.log(res.data);
+        this.productList = res.data
+      },
+      error:(err)=>{
+        console.log(err);
+        
+      }
+    });
+  }
 }
