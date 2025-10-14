df513a389cadfb988bd61de75514bf05243bda24
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-14 13:39:10 +0300

    Login

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../Core/Services/auth.service';
+import { Router } from '@angular/router';
+import { HttpRequest, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+ private readonly _authService= inject(AuthService);
+  private readonly _formBuilder = inject(FormBuilder);
+  private readonly _router = inject(Router);
+  msgError:string ="";
+    LgoinForm:FormGroup = this._formBuilder.group({
+      email : [null,[Validators.email ,Validators.required]],
+      password : [null,[Validators.required,Validators.pattern(/^\w{6,}$/)]],
+    });
+  LgoinFormSubmit():void {
+    if(this.LgoinForm.valid){
+      this._authService.setLgoinForm(this.LgoinForm.value).subscribe({
+        next:(res)=>{
+          if(res.message == 'success'){
+            this._router.navigate(['/home'])
+          }
+          console.log(res);
+        },
+        error:(err )=>{
+          this.msgError = err.error.message 
+          console.log(err);
+          
+        }
+      })
+    }    
+  }
 }
--- src/app/Core/Services/auth.service.ts
+++ src/app/Core/Services/auth.service.ts
@@ -1,13 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+   private baseUrl =  environment.baseUrl ;
 private readonly _httpClient = inject(HttpClient);
  setRegisterForm(data:object):Observable<any>{
- return this._httpClient.post("https://ecommerce.routemisr.com/api/v1/auth/signup",data)
+ return this._httpClient.post(`${this.baseUrl}/api/v1/auth/signup`,data)
+ }
+setLgoinForm(data:object):Observable<any>{
+ return this._httpClient.post(`${this.baseUrl}/api/v1/auth/signin`,data)
  }
 }
