2aef0ff0e0c15ba2d4a44a30f03e7e93fa685541
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-15 03:06:29 +0300

    headerInterceptor

--- src/app/Core/Services/cart.service.ts
+++ src/app/Core/Services/cart.service.ts
@@ -8,38 +8,25 @@ import { environment } from '../../../environments/environment';
 })
 export class CartService {
   private readonly _HttpClient = inject(HttpClient)
-  MyHeaders: any = { token: localStorage.getItem("UserToken") }
 
   addProducToCart(idProduct: string): Observable<any> {
     return this._HttpClient.post(`${environment.baseUrl}/api/v1/cart`,
       {
         "productId": idProduct
-      },
-      {
-        headers: this.MyHeaders
       }
     );
   }
   getLoggedUserCart(): Observable<any> {
-    return this._HttpClient.get(`${environment.baseUrl}/api/v1/cart`, {
-      headers: this.MyHeaders
-    });
+    return this._HttpClient.get(`${environment.baseUrl}/api/v1/cart`);
   }
   DeletCartItem(id: string): Observable<any> {
-    return this._HttpClient.delete(`${environment.baseUrl}/api/v1/cart/${id}`, {
-      headers: this.MyHeaders
-    });
+    return this._HttpClient.delete(`${environment.baseUrl}/api/v1/cart/${id}`);
   }
   UpdateCartProductQuantity(id: string ,newCount:number): Observable<any> {
     return this._HttpClient.put(`${environment.baseUrl}/api/v1/cart/${id}`,
-      { "count": newCount },
-      {
-        headers: this.MyHeaders
-      });
+      { "count": newCount });
   }
   ClearCartItem(): Observable<any> {
-    return this._HttpClient.delete(`${environment.baseUrl}/api/v1/cart`, {
-      headers: this.MyHeaders
-    });
+    return this._HttpClient.delete(`${environment.baseUrl}/api/v1/cart`);
   }
 }
--- src/app/Core/Services/orders.service.ts
+++ src/app/Core/Services/orders.service.ts
@@ -8,15 +8,11 @@ import { environment } from '../../../environments/environment';
 })
 export class OrdersService {
   private readonly _HttpClient = inject(HttpClient)
-  MyHeaders: any = { token: localStorage.getItem("UserToken") }
   CheckOut(idCart:string |null,shippingAddress:object):Observable<any>{
     return this._HttpClient.post(`${environment.baseUrl}/api/v1/orders/checkout-session/${idCart}?url=${environment.UrlServer}`,{
       
     "shippingAddress":shippingAddress
-    },
-      {
-        headers: this.MyHeaders
-      }
+    }
   )
   }
 }
